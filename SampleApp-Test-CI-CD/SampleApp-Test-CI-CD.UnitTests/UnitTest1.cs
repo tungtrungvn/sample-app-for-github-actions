4a34a448040ac7dd5a60490ecf29ca6cd8e130e6
Tung Nguyen <nguyenthanh.tung@petronas.com>
2024-12-23 20:29:56 +0700

    Init Project

---
+++ SampleApp-Test-CI-CD/SampleApp-Test-CI-CD.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SampleApp_Test_CI_CD.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
